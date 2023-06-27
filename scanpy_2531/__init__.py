4fdf1e972bebacba82081ec170ba580cc076aeab
Philipp A <flying-sheep@web.de>
2023-06-27 13:02:53 +0200

    init

---
+++ scanpy_2531/__init__.py
@@ -0,0 +1,16 @@
+import scanpy as sc
+
+def main():
+    adata = sc.datasets.pbmc3k()
+    adata.var_names_make_unique() 
+    sc.pp.filter_cells(adata, min_genes=200)
+    sc.pp.filter_genes(adata, min_cells=3)
+    adata.var['mt'] = adata.var_names.str.startswith('MT-')
+    sc.pp.calculate_qc_metrics(adata, qc_vars=['mt'], percent_top=None, log1p=False, inplace=True)
+    adata = adata[adata.obs.n_genes_by_counts < 2500, :]
+    adata = adata[adata.obs.pct_counts_mt < 5, :]
+    sc.pp.normalize_total(adata, target_sum=1e4)
+    sc.pp.log1p(adata)
+    sc.pp.highly_variable_genes(adata, min_mean=0.0125, max_mean=3, min_disp=0.5)
+    sc.tl.pca(adata, svd_solver='arpack')
+    print(adata.obsm["X_pca"])
